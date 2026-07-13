37588b17a32529af485087a1c1866a5385962c9d
Mnhatrealz <hhutieu0@gmail.com>
2026-07-13 19:14:31 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,14 +1,9 @@
 local placeId = game.PlaceId
 local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/"..game.PlaceId.."/universe")).universeId
-local Id = game.PlaceId
-local UniverseID = game.GameId
-if Id == 2753915549 or Id == 85211729168715 or Id == 4442272183 or Id == 79091703265657 or Id == 7449423635 or Id == 100117331123089 orId == 122478697296975 or UniverseID == 994732206 then  
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Mnhatrealz/-/refs/heads/main/BloxFruit.lua"))()
-end
+if placeId == 100117331123089 or placeId == 85211729168715 or placeId == 79091703265657 or placeId == 122478697296975 or UniverseID == 994732206 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Mnhatrealz/-/refs/heads/main/BloxFruit.lua"))()
 elseif placeId == 111989938562194 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Mnhatrealz/-/refs/heads/main/BrainrotEvolution.lua"))()
 elseif placeId == 97598239454123 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Mnhatrealz/-/refs/heads/main/MainGAG2.lua"))()
 end
-
